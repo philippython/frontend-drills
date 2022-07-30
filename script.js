744ace7233aa99b9748f9506eb9510819280186d
odulaja philip <odulajaphilip@gmail.com>
2022-07-30 14:27:44 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const header = document.querySelectorAll('h1')
+const header = document.querySelectorAll("h1");
 
-const refresh = function(val){
-  console.log(val)
+const refresh = function (val) {
+  console.log(val);
+};
+
+function declaration(string) {
+  console.log(string);
 }
-const add = function (val)
 
-refresh(header)
-add()
+const add = (val) => val + 23;
+
+refresh(header);
+add(12);
+declaration("Welcome to redux world");
